9e07504ddb1fc1b0d4bb27adb6111496b8c97ad3
wachcio <wachcio@poczta.fm>
2021-03-24 19:51:53 +0100

    working but not sort

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="result" v-if="vikingLotto">
+        <p
+            v-for="gameResult in vikingLotto.gameResult"
+            :key="gameResult.drawId"
+        >
+            {{ new Date(gameResult.drawDate).toLocaleDateString() }} -
+            {{ gameResult.winnerNumber[0].number }},{{
+                gameResult.winnerNumber[1].number
+            }},{{ gameResult.winnerNumber[2].number }},{{
+                gameResult.winnerNumber[3].number
+            }},{{ gameResult.winnerNumber[4].number }},{{
+                gameResult.winnerNumber[5].number
+            }}
+            <b>{{ gameResult.winnerNumber[6].number }}</b>
+        </p>
+    </div>
 </template>
 
 <script>
+const axios = require('axios');
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    name: 'HelloWorld',
+    data: function() {
+        return {
+            vikingLotto: null,
+        };
+    },
+    methods: {
+        async getData() {
+            return await axios
+                .get(
+                    'https://api.norsk-tipping.no/LotteryGameInfo/v1/api/results/vikinglotto',
+                    {
+                        params: {
+                            fromDate: '2019-01-01',
+                            toDate: `${new Date().getFullYear()}-${new Date().getMonth() +
+                                1}-${new Date().getDate()}`,
+                        },
+                    }
+                )
+                .then(function(response) {
+                    console.log(response.data);
+                    console.log(this.vikingLotto);
+
+                    this.vikingLotto = response.data;
+                    // return response;
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        },
+    },
+    mounted: function() {
+        // console.log(this.getData());
+
+        // this.vikingLotto = this.getData();
+        axios
+            .get(
+                'https://api.norsk-tipping.no/LotteryGameInfo/v1/api/results/vikinglotto',
+                {
+                    params: {
+                        fromDate: '2019-01-01',
+                        toDate: `${new Date().getFullYear()}-${new Date().getMonth() +
+                            1}-${new Date().getDate()}`,
+                    },
+                }
+            )
+            .then((response) => (this.vikingLotto = response.data));
+    },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+p,
+.result {
+    margin: 0;
+    padding: 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+p {
+    margin-bottom: 0.3em;
 }
 </style>
